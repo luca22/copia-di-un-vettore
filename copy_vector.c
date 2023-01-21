9ca0b4a14e4e667d1eaca1bd7381d71304873dff
Luca Lanzetta <78610431+luca22@users.noreply.github.com>
2023-01-21 12:24:50 +0100

    Add files via upload

---
+++ copy_vector.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+void copy_vector(uint32_t* dst, uint32_t* src, size_t n) {
+
+	if (src == NULL) {
+
+		return;
+	}
+
+	else {
+
+		for (size_t i = 0; i < n; i++) {
+
+			dst[i] = src[i];
+		}
+
+		return;
+	}
+
+}
